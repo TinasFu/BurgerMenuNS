574fc66f420a63fa6abc5dc9ada7b2a6c6cd2f8a
Tina Fu <TinasFu@outlook.com>
2014-11-25 15:30:55 -0800

    Initial Commit

---
+++ BurgerMenuNSTests/BurgerMenuNSTests.m
@@ -0,0 +1,40 @@
+//
+//  BurgerMenuNSTests.m
+//  BurgerMenuNSTests
+//
+//  Created by Shiquan Fu on 11/25/14.
+//  Copyright (c) 2014 Tina Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BurgerMenuNSTests : XCTestCase
+
+@end
+
+@implementation BurgerMenuNSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
